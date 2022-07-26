ef41b4bcb2406534cfe0993b5b037c835abe101b
Keigo Ando <anchan828@gmail.com>
2022-07-26 10:26:30 +0900

    refactor: easy add command

--- packages/cache-common/src/patch-more-commands.ts
+++ packages/cache-common/src/patch-more-commands.ts
@@ -1,48 +1,20 @@
-export function patchMoreCommands(self: any): void {
-  if (!self.store) {
+export function patchMoreCommands(cacheManager: any): void {
+  if (!cacheManager.store) {
     return;
   }
-
-  const storeName = typeof self.store === "string" ? self.store : self.store.name;
-
-  if (typeof self.store.hget === "function") {
-    self.hget = self.store.hget.bind(self.store);
-  } else {
-    printWarning(storeName, "hget");
-    self.hget = () => Promise.resolve();
-  }
-
-  if (typeof self.store.hset === "function") {
-    self.hset = self.store.hset.bind(self.store);
-  } else {
-    printWarning(storeName, "hset");
-    self.hset = () => Promise.resolve();
-  }
-
-  if (typeof self.store.hdel === "function") {
-    self.hdel = self.store.hdel.bind(self.store);
-  } else {
-    printWarning(storeName, "hdel");
-    self.hdel = () => Promise.resolve();
-  }
-
-  if (typeof self.store.hgetall === "function") {
-    self.hgetall = self.store.hgetall.bind(self.store);
-  } else {
-    printWarning(storeName, "hgetall");
-    self.hgetall = () => Promise.resolve();
-  }
-
-  if (typeof self.store.hkeys === "function") {
-    self.hkeys = self.store.hkeys.bind(self.store);
-  } else {
-    printWarning(storeName, "hkeys");
-    self.hkeys = () => Promise.resolve();
-  }
+  patchCommands(cacheManager, ["hget", "hset", "hdel", "hgetall", "hkeys"]);
 }
 
-function printWarning(storeName: string, commandName: string): void {
-  console.warn(
-    `This store '${storeName}' does not support ${commandName}. Note that calling it will not work correctly.`,
-  );
+function patchCommands(self: any, commands: string[]): void {
+  for (const command of commands) {
+    if (typeof self.store[command] === "function") {
+      self[command] = self.store[command].bind(self.store);
+    } else {
+      const storeName = typeof self.store === "string" ? self.store : self.store.name;
+      console.warn(
+        `This store '${storeName}' does not support ${command}. Note that calling it will not work correctly.`,
+      );
+      self[command] = () => Promise.resolve();
+    }
+  }
 }
